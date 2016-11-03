0a35808094c03f4cfd149021fa6fce763516c021
Guy Lafleur <kevin.netherton@gov.bc.ca>
2016-11-03 10:59:03 -0700

    more path resolutions

--- lib_int/Misc/__init__.py
+++ lib_int/Misc/__init__.py
@@ -1,8 +1,8 @@
-import os.path
-import sys
-import site
-
-externalPath = os.path.join(os.path.dirname(__file__), '..', 'externalLibs')
-externalPath = os.path.normpath(externalPath)
-site.addsitedir(externalPath)
-sys.path.insert(0, sys.path[len(sys.path) - 1])
+# import os.path
+# import sys
+# import site
+# 
+# externalPath = os.path.join(os.path.dirname(__file__), '..', 'externalLibs')
+# externalPath = os.path.normpath(externalPath)
+# site.addsitedir(externalPath)
+# sys.path.insert(0, sys.path[len(sys.path) - 1])
