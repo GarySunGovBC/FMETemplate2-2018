dfdafbca87bb4458cde6d1c27435477a53abf73a
GuyLafleur <kjnether@Matar.dmz>
2018-04-06 11:54:01 -0700

    working now in both test env and other env (FFSReader), next cleanup the mess that was created to try to get this working...

--- FFSReader.py
+++ FFSReader.py
@@ -10,8 +10,10 @@ import tempfile
 import shutil
 import subprocess
 import re
+import sys
 import DataBCFMWTemplate
 logger = logging.getLogger(__name__)
+
 
 def attemptFMEObjectsImport():
     global fmeobjects
@@ -20,16 +22,17 @@ def attemptFMEObjectsImport():
         import fmeobjects  # @UnresolvedImport
         logger.debug("Attempt 1: successfully imported the fmeobjects module")
     except:
-        # once move to production can extract the fme path from the 
+        # once move to production can extract the fme path from the
         # macro value FME_HOME and pass those parameters to this script
         #
-        
+
         pathList = os.environ['PATH'].split(';')
         sys.path.insert(0, r'E:\sw_nt\FME2015\fmeobjects\python27')
         os.environ['PATH'] = ';'.join(pathList)
         logger.debug("Attempt 2: importing the fmeobjects module")
         import fmeobjects  # @UnresolvedImport
         logger.debug("Attempt 2: successfully imported the fmeobjects module")
+
 
 class Reader(object):
     '''
@@ -51,17 +54,17 @@ class Reader(object):
             msg = 'you specified an FFS file: {0} that does not exist'
             msg = msg.format(self.ffsFile)
             self.logger.error(msg)
-        #attemptFMEObjectsImport()
-        
+        # attemptFMEObjectsImport()
+
     def getFeatureCountSameProcess(self):
         '''
-        bascially normal operation, just calls this method, it creates an 
+        bascially normal operation, just calls this method, it creates an
         ffs file reader counts the features and returns them
         :return: The number of features found in the FFS file.
         :rtype: int
         '''
         tempDir = os.path.dirname(self.ffsFile)
-        #ffsFile = tempfile.NamedTemporaryFile(dir=tempDir, delete=False)
+        # ffsFile = tempfile.NamedTemporaryFile(dir=tempDir, delete=False)
         ffsFile = tempfile.mktemp(suffix='.ffs', dir=tempDir, prefix='tmp_')
         self.logger.debug("tempfile name: %s", ffsFile)
         shutil.copyfile(self.ffsFile, ffsFile)
@@ -92,27 +95,27 @@ class Reader(object):
                 self.logger.debug("cleaning up the temp file: %s", ffsFile)
                 os.remove(ffsFile)
         return featureCnt
-    
+
     def getFeatureCountSeparateProcess(self):
         '''
         This method will spawn a separate subprocess calling this module using its
         command line interface.
-        
-        This functionality exists as the fme.exe crashes in the shutdown when 
+
+        This functionality exists as the fme.exe crashes in the shutdown when
         using the ffs file reader (fmeobjects) is attempted.
-        
-        To work around this limitation this method will create a subprocess and 
-        execute the ffs file reader on that subprocess, capturing and parsing the 
+
+        To work around this limitation this method will create a subprocess and
+        execute the ffs file reader on that subprocess, capturing and parsing the
         output from the ffs file
-        
+
         '''
         # parameters needed to do this:
         #   ffs file path
         #   python exe path
         #   path to this module __file__
         #
-        #TODO: replace this with a method call to calculate the path
-        #pythonexe = 'E:\sw_nt\python27\ArcGIS10.2'
+        # TODO: replace this with a method call to calculate the path
+        # pythonexe = 'E:\sw_nt\python27\ArcGIS10.2'
         # -----------------------------------------------------------------
         try:
             # this will get the arcpy paths, and add them to the sys.path
@@ -134,7 +137,46 @@ class Reader(object):
         my_env = os.environ.copy()
         pythonExe = os.path.join(pythonRootDir, 'python')
         self.logger.debug("executing this script in separate subprocess")
-        commandList = [pythonExe, __file__, self.ffsFile]
+
+        # making sure we are calling the .py file and not the pyc file
+        thisFileList = os.path.splitext(__file__)
+        thisFileList = list(thisFileList)
+        self.logger.debug("thisFileList1: %s", thisFileList)
+        if thisFileList[1] == '.pyc':
+            thisFileList[1] = '.py'
+        thisFileString = ''.join(thisFileList)
+        self.logger.debug("thisFileString: %s", thisFileString)
+        
+        # making sure all the template files are available
+        curDir = os.path.dirname(__file__)
+        libDir = os.path.join(curDir, 'lib')
+        self.logger.debug("syspath %s", sys.path)
+
+        # make sure all of sys.path is a regular string
+        sysPathStrList = []
+        for pth in sys.path:
+            sysPathStrList.append(str(pth))
+
+        syspathString = ';'.join(sysPathStrList)
+        self.logger.debug("syspath string %s, %s", syspathString, type(syspathString))
+        #my_env['PYTHONPATH'] = curDir + ';' + libDir + ';' + syspathString + ';' + my_env['PATH']
+        #my_env['PATH']       = curDir + ';' + libDir + ';' +  syspathString + ';' + my_env['PATH']
+        my_env['PYTHONPATH'] = r'\\data.bcgov\work\scripts\python\DataBCFmeTemplate2\lib'
+        
+        #for curEnv in my_env:
+        #    if not isinstance(my_env[curEnv], str):
+        #        self.logger.error("env that is not string is: %s", curEnv, type(my_env[curEnv]))
+                
+#         my_envList = my_env['PATH'].split(';')
+#         for curEnv in my_envList:
+#             self.logger.debug("env: %s", curEnv)
+#         self.logger.debug("------------------------------------------------")
+#         for curEnv in sys.path:
+#             self.logger.debug("sys path: {0}".format(curEnv))
+#         
+        self.logger.debug("myenv: %s", my_env)
+
+        commandList = [pythonExe, thisFileString, self.ffsFile]
         self.logger.debug("command being executed: %s", ' '.join(commandList))
         out = subprocess.check_output(commandList, env=my_env)
         self.logger.debug("out is: %s", out)
@@ -143,13 +185,13 @@ class Reader(object):
             self.logger.debug('outline: %s', outLine)
             if regexFfsFeatures.match(outLine):
                 features = outLine.replace(self.stdoutIDStr, '').strip()
-                
+
         self.logger.debug("features: %s", features)
         if not isinstance(features, (int, long, float, complex)):
             features = int(features)
 
         return features
-    
+
     def getFeatureCount(self, separateProcess=False):
         '''
         Uses FME Objects to read through the supplied FFS file and
@@ -157,7 +199,7 @@ class Reader(object):
 
         This only starts with what is possible.  Can actually get all
         the attributes etc from the file, geometry etc.
-        
+
         :var separateProcess: If this flag is set to true then the feature counting
                               will take place on a separate process than the process
                               that this method was called with.
@@ -180,7 +222,7 @@ class Reader(object):
 #             self.logger.debug("created the reader... now try reading")
 #             # reader.open(self.ffsFile, [])
 #             reader.open(ffsFile, [])
-# 
+#
 #             # Read all the features on the dataset.
 #             feature = reader.read()
 #             # atribNames = feature.getAllAttributeNames()
@@ -199,20 +241,21 @@ class Reader(object):
 #                 self.logger.debug("cleaning up the temp file: %s", ffsFile)
 #                 os.remove(ffsFile)
 #         return featureCnt
-    
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         usage = 'FFSReader <ffs file to read>'
         raise ValueError, usage
-    
+
     logger = logging.getLogger(__name__)
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.DEBUG)
-    
+
     ffsFile = sys.argv[1]
     rdr = Reader(ffsFile)
-    feats = rdr.getFeatureCountSeparateProcess()
-    #feats = rdr.getFeatureCount()
+    feats = rdr.getFeatureCountSameProcess()
+    # feats = rdr.getFeatureCount()
     outStr = rdr.stdoutTemplate.format(feats)
     print outStr
-    
+
